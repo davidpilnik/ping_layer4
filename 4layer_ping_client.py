02407fd9784e72ce2f3dc85d0058db5a77d141d8
David Pilnik <pilnik@post.bgu.ac.il>
2019-07-20 17:09:31 +0300

    support UDP, catch failures and print.

--- 4layer_ping_client.py
+++ 4layer_ping_client.py
@@ -1,66 +1,159 @@
+"""
+Program task
+creating client for ping over tcp/udp
+the program create client with "ping server socket"
+pass msg that contain port probing , timeout, protocol and size,
+the server should create a listener probe_socket for this port probing.
+the server will ack with 9999, mean that probe_socket is OK
+and finally the client will connect.
+(case of udp I write a simple echo to know that probing over udp is ok)
+"""
 import socket
 import sys
+import argparse
 
-PORT_SERVER = 5006
+
 EXPECTED_DATA_LEN = 100
-PROBING_PORT = 6006
+MSG_SERVER_READY = '9999'
+MSG_UDP_CLIENT = '8888'
+# PORT_SERVER = 5001
+# PROBING_PORT = 6006
+# IP_DEST_SERVER = 'localhost'
+# PING_TIMEOUT = 10
+# PING_PACKET_SIZE = 10
+# PING_PROTOCOL = 'tcp'
+CLIENT_UDP_IP_PORT = ('localhost', 60000)
+DEBUG = True
 
-IP_DEST_SERVER = 'localhost'
+
+def log(v):
+    if DEBUG:
+        print("[DEBUG]"+str(v))
 
 
-def create_socket_and_connect(ip_addr, port):
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def add_args():
+    parser = argparse.ArgumentParser(description='ping layer4 by probing port',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--server_addr', type=str, default='localhost', help="server listener address")
+    parser.add_argument('--server_port', type=int, default=5001, help="port server")
+    parser.add_argument('--port_probe', type=int, default=6001, help="port probe")
+    parser.add_argument('--timeout', type=int , default=15, help="timeout that client wait for server response")
+    parser.add_argument('--protocol', type=str, default='tcp', help="support tcp & udp")
+    parser.add_argument('--packet_size', type=int, default=10, help="unused")
+    return parser
 
-    # Connect the socket to the port where the server is listening
-    server_address = (ip_addr, port)
-    print "connecting to port: " + str(port) + " server_address %s" % server_address[0]
-    sock.connect(server_address)
+
+def create_socket_and_connect(ip_addr, port, timeout=30, protocol='tcp'):
+    sock = None
+    try:
+        if protocol == 'tcp':
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        elif protocol == 'udp':
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        log("Socket successfully created")
+
+    except socket.error as err:
+        print "[ERROR] Socket creation failed with error %s" % err
+        exit(-1)
+
+    try:
+        sock.settimeout(timeout)
+        # Connect the socket to the port where the server is listening
+        server_address = (ip_addr, port)
+        print "Connecting to %s " % str(server_address)
+        if protocol == 'tcp':
+            sock.connect(server_address)
+        elif protocol == 'udp':
+            # send msg=8888 and recv from server for make udp more reliable.
+            # todo set my ip and port not hard coded
+            sock.bind(CLIENT_UDP_IP_PORT)
+            sock.sendto(MSG_UDP_CLIENT, server_address)
+            data_udp_from_server = sock.recv(EXPECTED_DATA_LEN)
+
+            # udp test for reliabilty
+            if int(data_udp_from_server) != int(MSG_UDP_CLIENT):
+                print "[ERROR] udp ack data sent to server probe: %s not equal to recv: %s" % (MSG_UDP_CLIENT, data_udp_from_server)
+                return "[ERROR] udp ack data sent to server probe not equal to recv"
+
+            log("probing socket udp ACK is OK: %s" % data_udp_from_server)
+
+    except socket.error as err:
+        print "[ERROR] socket connect failed with error %s" % err
+        sock.close()
+        return err
 
     return sock
 
 
-def main():
+def run(server_addr, server_port, ping_port, ping_protocol, ping_timeout, ping_packet_size):
+
     # ========================
     # connect to server
     # ========================
-    sock = create_socket_and_connect(IP_DEST_SERVER, PORT_SERVER)
-
+    res_conn_probe = None
+    sock = create_socket_and_connect(server_addr, server_port, ping_timeout)
+    if not isinstance(sock, socket._socketobject):
+        exit(-1)
     try:
-        # ==================
-        # send probing port
-        # ==================
-        message = str(PROBING_PORT)
-        print 'sending "%s"' % message
+
+        # ===========================
+        # send probing port, protocol
+        # ===========================
+        message = str(ping_port) + '#' + str(ping_protocol) + '#' + str(ping_packet_size) + '#' + str(ping_timeout)
+        print "Sending %s .." % message
+
         sock.sendall(message)
 
         # =======================
         # wait server reponse
         # =======================
-
-        # after sending port probing, the server will send a msg to ack that now is listen to probe port,
-        # other, trigger timeout
-        # todo add timeout
         data = sock.recv(EXPECTED_DATA_LEN)
-        print "received data: %s" % data
+        # after sending port probing, the server will send a msg "9999" to ack that now is listen to probe port.
+        # case not response trigger timeout
+        # case data not equal 9999 the server send the error
 
-        # case recv fail
-        if data == 0:
-            # TODO this case
-            print("ERROR")
-            exit(2)
+        if data == MSG_SERVER_READY:
+            print "received server response: %s\n" \
+                  "server is ready, listen to probing port %s protocol %s \n" % (int(data), ping_port, ping_protocol)
+        else:
+            print "[ERROR:] server response not equal \'9999\'.\nServer is not ready," \
+                  " no listen to probing port %s error msg from SERVER: %s\nExit." % (ping_port, data)
+            exit(-1)
 
-        # ===========================
-        # connecting to probing port
-        # ===========================
-        sock_probe = create_socket_and_connect(IP_DEST_SERVER, PROBING_PORT)
-        # todo check sock probe failures
-        print "succes probing port: %s" % PROBING_PORT
-
-
-    finally:
-        print "closing socket"
+    except socket.error as err:
+        print "[ERROR] socket failed with error %s" % err
         sock.close()
+        exit(-1)
+
+    # ===========================
+    # connecting to probing port
+    # ===========================
+    res_conn_probe = create_socket_and_connect(server_addr, ping_port, ping_timeout, ping_protocol)
+
+    if isinstance(res_conn_probe, socket._socketobject):
+        print "SUCCESS! probing port: %s, protocol: %s \n" % (ping_port, ping_protocol)
+        log("Closing probe socket ..")
+        res_conn_probe.close()
+    else:
+        print "Fail! probing port, protocol %s : %s" % (ping_port, ping_protocol)
+        # print(str(res_conn_probe))
+    log("Closing socket")
+    if isinstance(sock, socket._socketobject):
+        sock.close()
+
+
+def main():
+    parser = add_args()
+    args = parser.parse_args()
+    log(args.server_addr)
+    log(args.server_port)
+    log(args.port_probe)
+    log(args.protocol)
+    log(args.timeout)
+    log(args.packet_size)
+
+    # todo check server ip adderss is a real ip
+    run(args.server_addr, args.server_port, args.port_probe, args.protocol, args.timeout, args.packet_size)
 
 
 if __name__ == '__main__':
--- 4layer_ping_server.py
+++ 4layer_ping_server.py
@@ -3,81 +3,185 @@ Program goal:
 support 4layer ping
 How is work:
 The program is a listener server, that waiting for client connection.
-the first msg receive is a "port probing", in other words,
-this server will use this port number and create addition listener on this port for probing.
-if client succsess to connect to probing port, its mean ping success, other, not success.(i.e: port is block by iptable)
+the first msg receive contain "port probing", protocol and size, in other words,
+this server will use this port number and create addition listener on this port - for probing.
+if client success to connect to probing port, its mean ping success, other, fail (i.e: port is block by iptable).
 """
 import socket
+import time
 import sys
+import argparse
 # import threading
-PORT_LISTEN = 5006
-ADDRESS_LISTEN = 'localhost'
+# PORT_LISTEN = 5001
+# ADDRESS_LISTEN = 'localhost'
+DEBUG = True
+
+def log(v):
+    if DEBUG:
+        print("[DEBUG]"+str(v))
 
 
-def create_socket_and_bind(ip_addr, port):
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def add_args():
+    parser = argparse.ArgumentParser(description='ping layer4 server',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--server_addr', type=str, default='localhost', help="server listener address")
+    parser.add_argument('--server_port', type=int, default=5001, help="port server")
+    return parser
 
-    # just DEBUG: permits reusing address
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    # Bind the socket to the port
-    server_address = (ip_addr, port)
-    sock.bind(server_address)
+def parser_data(data):
+    # TODO casew parser fail
+    data = data.split('#')
+    return data[0], data[1], data[2], data[3]
+
+
+def create_socket_and_bind(ip_addr, port, protocol='tcp', timeout=None):
+    """
+    At start function try to create socket accoding params, if fail: exit code.
+    case creation socket success: the function try to bind port,
+    case bind succeed - return socket obj bind,
+    case fail, return string error socket.
+
+    :param ip_addr:
+    :param port:
+    :param protocol:
+    :return: socket obj or socket error msg
+    """
+    sock = None
+    try:
+        if protocol == 'tcp':
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            if timeout is not None:
+                sock.settimeout(timeout)
+            if DEBUG:
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        elif protocol == 'udp':
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            if timeout is not None:
+                sock.settimeout(timeout)
+            if DEBUG:
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        log("Socket protocol %s successfully created" % protocol)
+
+    except socket.error as err:
+        print "socket creation failed with error %s" % err
+        exit(-1)
+
+    try:
+        # Bind the socket to the port
+        log("socket bind succeed")
+        server_address = (ip_addr, port)
+        sock.bind(server_address)
+
+    except socket.error as err:
+        print "socket bind failed with error %s" % err
+        sock.close()
+        return err
 
     return sock
 
 
-def main():
+def run(server_addr, server_port):
+    sock_wel = None
+    sock_probe = None
+
     # create welcome socket
-    sock_wel = create_socket_and_bind(ADDRESS_LISTEN, PORT_LISTEN)
+    sock_wel = create_socket_and_bind(server_addr, server_port)
+    if not isinstance(sock_wel, socket._socketobject):
+        exit(-1)
+
     sock_wel.listen(100)
 
     while True:
-        # =======================
-        # Wait for a connection
-        # =======================
-        print "server listen on port :" + str(PORT_LISTEN)
-        connection, client_address = sock_wel.accept()
+        try:
+            # ===================================
+            # Welcome socket wait for a connection
+            # ===================================
+            print "Server listen on ip: " + str(server_addr) + ", port: " + str(server_port)
+            connection, client_address = sock_wel.accept()
 
-        if connection:
-            # ====================
-            # receive port probing
-            # ====================
-            data = connection.recv(1000)
-            print 'received data: "%s"' % data
-            print("try to listen to probing port")
-            port_probe = int(data)
+            if connection:
+                # ============================================
+                # receive [port, protocol, timeout] for  probing
+                # ============================================
+                data = connection.recv(1000)
+                port_probe, protocol_probe, size_probe, timeout_probe = parser_data(data)
+                print "Data recv: port_probe: %s protocol_probe %s size_probe %s timeout_probe %s " \
+                      % (port_probe, protocol_probe, size_probe, timeout_probe)
 
-            # =========================================
-            #  create listener on port probing
-            # =========================================
+                # =========================================
+                #  create listener on port probing
+                # =========================================
+                print "Creating socket for probing port"
+                sock_probe = create_socket_and_bind(server_addr, int(port_probe), protocol_probe, int(timeout_probe))
 
-            sock_probe = create_socket_and_bind(ADDRESS_LISTEN, port_probe)
-            # todo if socket probe not create because problem need to send error msg to client
+                if isinstance(sock_probe, socket._socketobject):
+                    # case socket probe is ok
+                    if protocol_probe == 'tcp':
+                        sock_probe.listen(10)
 
-            sock_probe.listen(100)
+                    # ============================================
+                    # ack client that probe socket listen
+                    # ============================================
+                    print "Sending msg=9999 to client that probe_port on listen"
+                    connection.sendall("9999")
 
-            # ============================================
-            # ack client that probe socket listen
-            # ============================================
-            print "sending msg to client that probe_port on listen"
-            connection.sendall("success to listen probing port")
+                    # ===============================
+                    # wait client connection &
+                    # report connection status
+                    # ================================
+                    if protocol_probe == 'tcp':
+                        conne_probe, client_address_probe = sock_probe.accept()
+                        if conne_probe:
+                            log("Accept probe_sock from client_addr: %s" % str(client_address))
+                            print "SUCCESS! probing port: %s, protocol: %s" % (str(port_probe), str(protocol_probe))
+                            sock_probe.close()
+                        else:
+                            # case when not success to probe
+                            print "[ERROR]: connection to probe port FAIL"
 
-            # ===============================
-            # wait client connection &
-            # report connection status
-            # ================================
-            conne_probe, client_address_probe = sock_probe.accept()
-            if conne_probe:
-                print "accept probe sock: client_addr %s" % str(client_address)
-                print "probing port %s success" % str(port_probe)
+                    elif protocol_probe == 'udp':
+                        udp_data , udp_client_addr= sock_probe.recvfrom(int(size_probe))
+                        print "SUCCESS! probing port %s , protocol %s" % (str(port_probe), str(protocol_probe))
+                        log("Sending \'udp ack\' to client")
+                        sock_probe.sendto(udp_data, udp_client_addr)
+                        pass
+                else:
+                    # case socket probe FAIL
+                    print "Sending error msg to client that probe_port FAIL!"
+                    connection.sendall(str(sock_probe))
+
+        except KeyboardInterrupt:
+            sys.exit(-3)
+
+        except BaseException as e:
+            print "[ERROR] SERVER CRASH! " + str(e)
+
+            if isinstance(sock_probe, socket._socketobject):
                 sock_probe.close()
-            else:
-                # case when not success to probe
-                print "error: connection to probe port FAIL"
-                print "TODO!"
+            if not isinstance(sock_wel, socket._socketobject):
+                # create welcome socket
+                sock_wel = create_socket_and_bind(server_port, server_port)
+                if not isinstance(sock_wel, socket._socketobject):
+                    exit(-1)
+                sock_wel.listen(100)
+
+
+def main():
+    parser = add_args()
+    args = parser.parse_args()
+
+    log(args.server_addr)
+    log(args.server_port)
+
+    run(args.server_addr, args.server_port)
 
 
 if __name__ == '__main__':
     main()
+
+
+# while True:
+#     time.sleep(1)
